776bedfc52fe94d42adce774053512300be734ea
Ardhi Nur Fauzi <ar.dhi950@gmail.com>
2020-12-23 03:12:54 +0700

    Update SiswaController.php

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Siswa;
 
 class SiswaController extends Controller
 {
-    //
+    public function index()
+    {
+        return Siswa::all();
+    }
+
+    public function create(request $request)
+    {
+        $siswa  = new Siswa;
+        $siswa->nama    =   $request->nama;
+        $siswa->alamat  =   $request->alamat;
+        $siswa->save();
+
+        return "Data berhasil di tambahkan";
+    }
 }
